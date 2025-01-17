ba6cc8b152b40ea629c1e3a2c986d9bb3dccce4e
RaihanIslamRauf <md.raufislam27@gmail.com>
2025-01-18 00:59:14 +0600

    added homepage banner image animation

--- src/pages/Home/Banner.jsx
+++ src/pages/Home/Banner.jsx
@@ -1,15 +1,25 @@
 import { delay, easeOut } from "motion";
 import React from "react";
 import { motion } from "framer-motion";
+import team1 from "../../assets/team/team1.jpg"
+import team2 from "../../assets/team/team2.jpg"
 
 const Banner = () => {
   return (
     <div className="hero bg-base-200 min-h-96">
       <div className="hero-content flex-col lg:flex-row-reverse">
         <div className="flex-1">
-          <img
-            src="https://img.daisyui.com/images/stock/photo-1635805737707-575885ab0820.webp"
-            className="max-w-sm rounded-lg shadow-2xl"
+          <motion.img
+            src={team1}
+            animate = {{y:[50,100,50]}}
+            transition = {{duration:10, repeat: Infinity}}
+            className="max-w-sm w-64 rounded-t-[40px] rounded-br-[40px] border-l-4 border-b-4 border-blue-400 rounded-lg shadow-2xl"
+          />
+          <motion.img
+            src={team2}
+            animate = {{x:[100,150,100]}}
+            transition = {{duration:10, delay:5, repeat: Infinity}}
+            className="max-w-sm w-64 rounded-t-[40px] rounded-br-[40px] border-l-4 border-b-4 border-blue-400 rounded-lg shadow-2xl"
           />
         </div>
         <div className="flex-1">
