784b057e869578db3fc0c13335c50e7d121d5152
RaihanIslamRauf <md.raufislam27@gmail.com>
2025-01-25 02:01:09 +0600

    created job application form

--- src/pages/JobDetails/JobDetails.jsx
+++ src/pages/JobDetails/JobDetails.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import { useLoaderData } from 'react-router-dom';
+import { Link, useLoaderData } from 'react-router-dom';
 
 const JobDetails = () => {
     
-    const {title, company, description} = useLoaderData();
+    const {_id, title, company, description} = useLoaderData();
     
 
     return (
@@ -11,7 +11,7 @@ const JobDetails = () => {
             <h2 className='text-3xl'>job details for {title}</h2>
             <p>apply for: {company}</p>
             <p>description: {description}</p>
-            <button className='btn btn-primary mt-2'>Apply Now</button>
+            <Link to={`/jobApply/${_id}`}><button className='btn btn-primary mt-2'>Apply Now</button></Link>
         </div>
     );
 };
