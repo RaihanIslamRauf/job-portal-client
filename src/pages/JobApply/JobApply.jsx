7153e30883d58f8890ce45158d6d657678bf8ce8
RaihanIslamRauf <md.raufislam27@gmail.com>
2025-01-27 06:52:32 +0600

    created custom hook and used it

--- src/pages/JobApply/JobApply.jsx
+++ src/pages/JobApply/JobApply.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
+import useAuth from '../../hooks/useAuth';
 
 const JobApply = () => {
 
     const {id} = useParams();
-    console.log(id);
+    const {user} = useAuth();
+    // console.log(id, user);
 
     const submitJobApplication = e =>{
         e.preventDefault();
@@ -12,21 +14,21 @@ const JobApply = () => {
         const linkedIn = form.linkedIn.value;
         const github = form.github.value;
         const resume = form.resume.value;
-        console.log(linkedIn, github, resume);
+        // console.log(linkedIn, github, resume);
 
     }
 
+    const jobApplication = {
+      job_id: id,
+      applicant_email: user.email,
+      linkedIn,
+      github,
+      resume
+    }
+
     return (
-        <div className="hero bg-base-200 min-h-screen">
-        <div className="hero-content flex-col lg:flex-row-reverse">
-          <div className="text-center lg:text-left">
-            <h1 className="text-5xl font-bold">Login now!</h1>
-            <p className="py-6">
-              Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
-              quasi. In deleniti eaque aut repudiandae et a id nisi.
-            </p>
-          </div>
-          <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+          <div className="card bg-base-100 w-full shadow-2xl">
+          <h1 className="text-5xl font-bold text-center">Apply Job and Good Luck!</h1>
             <form onSubmit={submitJobApplication} className="card-body">
               <div className="form-control">
                 <label className="label">
@@ -54,8 +56,6 @@ const JobApply = () => {
               </div>
             </form>
           </div>
-        </div>
-      </div>
     );
 };
 
